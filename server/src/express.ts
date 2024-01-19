7441b580d01c302923bc41a50c25002720e22f33
Dream027 <amritpalsinghgmsm3@gmail.com>
2024-01-19 03:50:42 +0000

    aded public folder in express app

--- server/src/express.ts
+++ server/src/express.ts
@@ -11,16 +11,17 @@ app.use(
         origin: "http://localhost:3000",
         methods: ["GET", "POST", "PUT", "DELETE"],
         // credentials: true,
-    }),
+    })
 );
 app.use(express.json());
 app.use(
     express.urlencoded({
         limit: "25mb",
         extended: true,
-    }),
+    })
 );
 app.use(cookieParser());
+app.static("public");
 
 // Routes
 app.use("/api/v1/users", usersRoute);
