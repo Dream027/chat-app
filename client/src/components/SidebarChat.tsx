883b14a640c95ab716ba8d7c58ae3e8710b5acd9
Dream027 <amritpalsinghgmsm3@gmail.com>
2024-01-22 13:23:38 +0530

    added navbar

---
+++ client/src/components/SidebarChat.tsx
@@ -0,0 +1,3 @@
+export default function SidebarChat() {
+    return <div>Sidebar</div>;
+}
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Navbar from "@/components/Navbar";
+import { Inter } from "next/font/google";
+import SidebarChat from "@/components/SidebarChat";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <main className={inter.className}>
+            <Navbar />
+            <Component {...pageProps} />
+        </main>
+    );
 }
