8c940abb102f2b7fddea4339a60b5aa2d35e8165
Dream027 <amritpalsinghgmsm3@gmail.com>
2024-03-01 13:39:26 +0530

    fix: middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -2,15 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { fetchClient } from "@/utils/fetchClient";
 
 export default async function middleware(req: NextRequest) {
-    // const res = await fetchClient("/users/session", "GET", null, {
-    //     headers: {
-    //         Authorization: `Bearer ${req.cookies.get("token")?.value}`,
-    //     },
-    // });
-    // if (!res?.success) {
-    //     return NextResponse.redirect(new URL("/signin", req.nextUrl));
-    // }
-    // return NextResponse.next();
+    const res = await fetchClient("/users/session", "GET", null, {
+        headers: {
+            Authorization: `Bearer ${req.cookies.get("token")?.value}`,
+        },
+    });
+    if (!res?.success && req.nextUrl.pathname !== "/signin") {
+        return NextResponse.redirect(new URL("/signin", req.nextUrl));
+    } else if (res?.success && req.nextUrl.pathname === "/signin") {
+        return NextResponse.redirect(new URL("/", req.nextUrl));
+    }
+    return NextResponse.next();
 }
 
 export const config = {
